980ec685bc3fada5b35d736a9f0cd872ac301c16
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-29 03:29:01 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+require_relative 'item.rb'
+require 'pry'
+
+class Application
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      # if @@items.any? {|i| i.name == req.path.split("/").last}
+      match = @@items.find{|i| i.name == req.path.split("/").last}
+      if match
+        resp.write "Price: #{match.price}\n"
+      else
+        resp.status = 400
+        resp.write "Item not found\n"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found\n"
+    end
+
+    resp.finish
+  end
+end
